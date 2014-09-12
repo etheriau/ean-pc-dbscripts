1ade99bb0b173df942aaa7526f988359ca00da77
Jon Arce <jon.arce@gmail.com>
2014-09-11 19:22:35 -0500

    Updates to EAN Gepgraphic Regions
    updates to documents and query

--- queries/Regions/Regions - autocomplete example.sql
+++ queries/Regions/Regions - autocomplete example.sql
@@ -23,7 +23,7 @@ SELECT eanprod.REGION_NAME_CLEAN(RegionNameLong) AS 'English',parentregionlist.R
        CenterLatitude AS 'Latitude',CenterLongitude AS 'Longitude',
 	   eanprod.HOTELS_IN_REGION_COUNT(parentregionlist.RegionID) AS 'EANHotelIDCount',
 	   eanprod.HOTELS_IN_REGION(parentregionlist.RegionID) AS 'EANHotelIDList',
-	   'Cities/Area' AS 'DisplayType'
+	   'Cities/Areas' AS 'DisplayType'
 FROM eanprod.parentregionlist
 LEFT JOIN eanprod.regioncentercoordinateslist
 ON parentregionlist.RegionID = regioncentercoordinateslist.RegionID
@@ -56,14 +56,17 @@ UNION ALL
 SELECT RegionNameLong AS 'English',RegionID,Latitude,Longitude,
 	   eanprod.HOTELS_IN_REGION_COUNT(pointsofinterestcoordinateslist.RegionID) AS 'EANHotelIDCount',
 	   eanprod.HOTELS_IN_REGION(pointsofinterestcoordinateslist.RegionID) AS 'EANHotelIDList',
-	   'Landmark' AS 'DisplayType'  
+	   'Landmarks' AS 'DisplayType'  
 FROM eanprod.pointsofinterestcoordinateslist
 
 UNION ALL
 ## the Hotels Records
-SELECT  Name AS 'English',NULL AS 'EANREgionID',Latitude,Longitude,
+SELECT  Name AS 'English',RegionID AS 'EANRegionID',Latitude,Longitude,
 	   1 AS 'EANHotelIDCount',EANHotelID AS 'EANHotelIDList','Hotels' AS 'DisplayType' 
 FROM eanprod.activepropertylist
+WHERE SupplierType='ESR'
 ) AS T
 # just add the ones we can actually have hotels to sell
-WHERE EANHotelIDList > 0;
+WHERE EANHotelIDList > 0
+# Order it by the primary language to use
+ORDER BY English;
